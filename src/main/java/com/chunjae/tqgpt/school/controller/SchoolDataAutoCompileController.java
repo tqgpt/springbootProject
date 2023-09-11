795a9d905fd2afe7e959f0f9a4a230a477907d45
starMinK <rbals040329@gmail.com>
2023-09-11 17:07:05 +0900

    :sparkles: Feat: "niceAPI에서 데이터 받아오기"

---
+++ src/main/java/com/chunjae/tqgpt/school/controller/SchoolDataAutoCompileController.java
@@ -0,0 +1,18 @@
+package com.chunjae.tqgpt.school.controller;
+
+import com.chunjae.tqgpt.school.service.SchoolDataAutoCompileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SchoolDataAutoCompileController {
+
+    private final SchoolDataAutoCompileService schoolDataAutoCompileService;
+
+    @PostMapping("/schoolDataCompile")
+    public void setSchoolData() {
+        schoolDataAutoCompileService.upsertSchoolData();
+    }
+}
