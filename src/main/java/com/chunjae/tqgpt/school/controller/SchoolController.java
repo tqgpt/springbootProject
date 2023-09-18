49c594b50eccca260f09f78c83eba9674504a588
jhreposi <jhsygo@gmail.com>
2023-09-18 11:31:42 +0900

    fix : add modify School - 경로, 정보구조, css변경

--- src/main/java/com/chunjae/tqgpt/school/controller/SchoolController.java
+++ src/main/java/com/chunjae/tqgpt/school/controller/SchoolController.java
@@ -33,7 +33,7 @@ public class SchoolController {
      * */
     @GetMapping("/add")
     public String addSchool() {
-        return "addSchool";
+        return "views/schoolManage/addSchool";
     }
 
     /*학교 정보 추가 페이지
@@ -59,7 +59,7 @@ public class SchoolController {
 
         model.addAttribute("info", getSchool.get());
 
-        return "modifySchool";
+        return "views/schoolManage/modifySchool";
     }
 
     @PostMapping("/api/modify")
